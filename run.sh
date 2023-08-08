a2594a1c0d160e8d81506ed4737c065f0490da67
Cell Wizard <69512249+CellWizard@users.noreply.github.com>
2023-08-07 17:05:56 -0700

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ PLUTO_IP="$1"
 scp system_top.bit.bin root@"$PLUTO_IP":/lib/firmware/
 scp loadplutobluez_runme_on_pluto.txt root@"$PLUTO_IP":/configurepluto.sh
 ssh root@"$PLUTO_IP" "ash /configurepluto.sh"
-IIOD_REMOTE=ip:"$1" iio_readdev -T 100000 -b 4000000 -s 128000000 cf-ad9361-lpc voltage0 voltage1 | ../plutoblueconverter/target/release/plutoblueconverter  | ./apps/btrx -s -i - -f 2440000000.0 -S -r 84000000 -t 10
+IIOD_REMOTE=ip:"$1" iio_readdev -T 100000 -b 4000000 -s 128000000 cf-ad9361-lpc voltage0 voltage1 | ./plutoblueconverter/target/release/plutoblueconverter  | ./apps/btrx -s -i - -f 2440000000.0 -S -r 84000000 -t 10
