053b4a7af016c0647e9fbac74176b85b9a49cf3d
Cell Wizard <69512249+CellWizard@users.noreply.github.com>
2023-08-11 14:57:33 -0700

    fix arrayfire

--- runfull.sh
+++ runfull.sh
@@ -2,4 +2,4 @@ PLUTO_IP="$1"
 cat system_top.bit.bin | ssh root@"$PLUTO_IP" "cat - > /lib/firmware/system_top.bit.bin"
 cat loadplutobluez_runme_on_pluto.txt | ssh root@"$PLUTO_IP" "cat - > /configurepluto.sh"
 ssh root@"$PLUTO_IP" "ash /configurepluto.sh"
-IIOD_REMOTE=ip:"$1" iio_readdev -T 100000 -b 4000000 -s 128000000 cf-ad9361-lpc voltage0 voltage1 | ./plutoblueconverter/target/release/plutoblueconverter  | ./apps/btrx -s -i - -f 2440000000.0 -S -r 84000000 -t 17
+IIOD_REMOTE=ip:"$1" iio_readdev -T 100000 -b 4000000 -s 128000000 cf-ad9361-lpc voltage0 voltage1 | LD_PRELOAD=/usr/lib/libafcpu.so AF_MAX_BUFFERS=10 POCL_MAX_THREAD_COUNT=12 ./plutoblueconverter/target/release/plutoblueconverter  | ./apps/btrx -s -i - -f 2440000000.0 -S -r 84000000 -t 17
